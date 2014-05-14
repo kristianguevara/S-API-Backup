9210f7d4a21df7bfdf942a2d030f3d809693d9c1
Kristian Guevara <xtian.kristian@hotmail.com>
2014-05-14 15:33:34 +0800

    Update SteamPlayerClass.cs

--- SteamAPI Try - MVC/JsonClass/SteamPlayerClass.cs
+++ SteamAPI Try - MVC/JsonClass/SteamPlayerClass.cs
@@ -7,20 +7,6 @@ namespace SteamAPI_Try___MVC.JsonClass
 {
     public class SteamPlayerClass
     {
-        /*
-         "steamid": "76561198131281243",
-                 "communityvisibilitystate": 3,
-                 "personaname": "xtian.kristian",
-                 "profileurl": "http://steamcommunity.com/profiles/76561198131281243/",
-                 "avatar": "http://media.steampowered.com/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb.jpg",
-                 "avatarmedium": "http://media.steampowered.com/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg",
-                 "avatarfull": "http://media.steampowered.com/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
-                 "personastate": 0,
-                 "primaryclanid": "103582791429521408",
-                 "timecreated": 1396063627,
-                 "personastateflags": 0
-         */
-
         
         public class Rootobject
         {
